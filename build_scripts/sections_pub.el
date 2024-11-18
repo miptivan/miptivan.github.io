2c2b71acaa239255dffe8f83552eab18b00faba6
miptivan <miptivan@gmail.com>
2024-11-18 23:09:15 +0500

    edit links

--- build_scripts/sections_pub.el
+++ build_scripts/sections_pub.el
@@ -2,19 +2,19 @@
 
 (defun my/preamble-gen (p) (concat
                             "<nav> <center>"
-                            (my/link-gen "index.html" "Home")
-                            (my/link-gen "about.html" "About")
+                            (my/link-gen "https://miptivan.github.io/index.html" "Home")
+                            (my/link-gen "https://miptivan.github.io/about.html" "About")
                             (my/link-gen "https://github.com/miptivan" "Github")
-                            (my/link-gen "analysis.html" "Analysis")
+                            (my/link-gen "https://miptivan.github.io/analysis.html" "Analysis")
                             "</center> </nav>"
                             ))
 
 (defun my/postamble-gen (p) (concat
                               "<footer> <center>"
-                              (my/link-gen "index.html" "Home")
-                              (my/link-gen "about.html" "About")
-                              (my/link-gen "https://github.com/miptivan" "Github")
-                              (my/link-gen "analysis.html" "Analysis")
+                            (my/link-gen "https://miptivan.github.io/index.html" "Home")
+                            (my/link-gen "https://miptivan.github.io/about.html" "About")
+                            (my/link-gen "https://github.com/miptivan" "Github")
+                            (my/link-gen "https://miptivan.github.io/analysis.html" "Analysis")
                             "</center> </nav>"
                               "</center> </footer>"
                             ))
