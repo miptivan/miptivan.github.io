e73df05088e5519415954d55afc13371de4bc61b
miptivan <miptivan@gmail.com>
2024-11-18 22:41:28 +0500

    Initial commit

---
+++ build_pub.el
@@ -0,0 +1,24 @@
+(add-to-list 'load-path (expand-file-name "~/blog_pub/build_scripts" (file-name-directory load-file-name)))
+
+(load "init_straight_pub")
+(load "org_setup")
+(load "sections_pub")
+
+(require 'ox-publish)
+
+  (setq org-publish-project-alist
+        (list
+          (list "public"
+              :recursive t
+              :base-directory "./content"
+              :base-extension "org"
+              :publishing-directory "./public"
+              :publishing-function 'org-html-publish-to-html
+              :html-preamble (my/preamble-gen "." )
+              :html-postamble (my/postamble-gen ".")
+              :with-title nil)
+              ))
+
+(org-publish-all t)
+;; (org-publish-project "public")
+(message "Build complete!")
---
+++ build_scripts/sections_pub.el
@@ -0,0 +1,21 @@
+(defun my/link-gen (fname label) (format "[ <a href=\"%s\"/> %s </a> ]" fname label))
+
+(defun my/preamble-gen (p) (concat
+                            "<nav> <center>"
+                            (my/link-gen "index.html" "Home")
+                            (my/link-gen "about.html" "About")
+                            (my/link-gen "https://github.com/miptivan" "Github")
+                            (my/link-gen "analysis.html" "Analysis")
+                            "</center> </nav>"
+                            ))
+
+(defun my/postamble-gen (p) (concat
+                              "<footer> <center>"
+                              (my/link-gen "index.html" "Home")
+                              (my/link-gen "about.html" "About")
+                              (my/link-gen "https://github.com/miptivan" "Github")
+                              (my/link-gen "analysis.html" "Analysis")
+                            "</center> </nav>"
+                              "</center> </footer>"
+                            ))
+
